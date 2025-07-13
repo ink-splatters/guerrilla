447115bc207034b53d044f1e6813a59b83120e98
Peter A. <ink.splatters@pm.me>
2025-07-13 20:50:02 +0200

    version up

--- nix/flake-module.nix
+++ nix/flake-module.nix
@@ -11,7 +11,7 @@
   }: {
     packages.guerrilla = pkgs.buildGo124Module rec {
       pname = "guerrilla";
-      version = "20250606";
+      version = "20250713";
 
       inherit (config) src;
       vendorHash = "sha256-oiWWLKY2SbUqArxuDB5W2Tl2jn2Wy5XwKm9TWH7ho60=";
